7ff3c74a03e300274d0c0188d0fbd4b313c0d5ff
sraghavendra <raghavendrasasula@gmail.com>
2021-01-27 12:21:27 +0530

    Add project files.

---
+++ SimpleInterest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SimpleInterest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //variable declaration
+                double Amount, Interest, SimpleInterest, change, finalAmountMonth, AnnualInterest, finalAmountAnnum;
+
+                Console.WriteLine("Enter Amount :");
+                Amount = Convert.ToDouble(Console.ReadLine());
+
+                change = Amount / 100;
+
+                Console.WriteLine("Enter Rate of Interest(%) :");
+                Interest = Convert.ToDouble(Console.ReadLine());
+
+                // Monthly Interest formula
+                SimpleInterest = (change * Interest);
+                // Annum Interest formula
+                AnnualInterest = (SimpleInterest * 12);
+
+                finalAmountMonth = (SimpleInterest + Amount);
+                finalAmountAnnum = (AnnualInterest + Amount);
+
+                Console.WriteLine("Interest Earned for a Month: {0}", SimpleInterest);
+                Console.WriteLine("Interest Earned for an Annum : {0}", AnnualInterest);
+                Console.WriteLine("Final Amount after Interest for a Month : {0}", finalAmountMonth);
+                Console.WriteLine("Final Amount after Interest for an Annum : {0}", finalAmountAnnum);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
